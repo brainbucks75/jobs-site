433d70a3fa05c4109bf9b4e4c06ba164e7d72aa0
brainbucks75 <brainbucks75@gmail.com>
2026-08-12 08:17:58 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1794,18 +1794,19 @@ const colors = {
 
 const theme = colors[data.cvColor] || colors.navy;
 
-console.log('PUPPETEER EXECUTABLE:', puppeteer.executablePath());
+const executablePath = await puppeteer.executablePath();
+
+console.log('PUPPETEER EXECUTABLE:', executablePath);
 
 const browser = await puppeteer.launch({
   headless: true,
-  executablePath: puppeteer.executablePath(),
+  executablePath: executablePath,
   args: [
     '--no-sandbox',
     '--disable-setuid-sandbox',
     '--disable-dev-shm-usage'
   ]
 });
-
     const page = await browser.newPage();
 
     await page.setContent(`
